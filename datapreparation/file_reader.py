c454517a666d39a9a434508375bda1ac5e09804e
Alan Achaval <achaval.alan.n@gmail.com>
2019-07-07 18:26:57 -0300

    Datos desde los archivos hea

--- datapreparation/file_reader.py
+++ datapreparation/file_reader.py
@@ -1,5 +1,7 @@
 import os
+
 import numpy as np
+import pandas as pd
 
 from signal.signal import Signal
 
@@ -10,12 +12,19 @@ class FileReader:
         self.data_folder = data_folder
 
     def __iter__(self):
-        return map(self.read_file, os.listdir(self.data_folder))
+        return map(self.read_file, filter(lambda x: x.endswith('.hea'), os.listdir(self.data_folder)))
 
-    def read_file(self, filename):
-        path = os.path.join(self.data_folder, filename)
-        values = np.fromfile(path)
-        # https://physionet.org/physiobank/database/aami-ec13/
-        # Each recording contains one ECG signal sampled at 720 Hz with 12-bit resolution
-        fs = 720
-        return Signal(filename, values, fs)
+    def read_file(self, header_name):
+        # Example of a header file:
+        #
+        # aami4a 1 720 65211
+        # aami4a.dat 16 80 12 2048 2040 15234 0 ECG
+        # # ANSI/AAMI EC13-1992, Figure 4a, 1x.
+        header_path = os.path.join(self.data_folder, header_name)
+        header = pd.read_csv(header_path, sep=" ", names=[0, 1, 2, 3, 4, 5, 6, 7, 8])
+        data_name = header.loc[1][0]
+        data_path = os.path.join(self.data_folder, data_name)
+        data = np.fromfile(data_path)
+        fs = int(header.loc[0][2])
+        signal_name = header[0][0]
+        return Signal(signal_name, data, fs)
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import numpy as np
 import matplotlib.pyplot as plt
+import numpy as np
 
 from datapreparation.file_reader import FileReader
 
