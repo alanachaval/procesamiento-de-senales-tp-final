835e9c2a092644f8209bdc07bc09ed0907675efd
Alan Achaval <achaval.alan.n@gmail.com>
2019-07-07 18:41:06 -0300

    Linea de tiempo calculada por la clase Signal

--- main.py
+++ main.py
@@ -1,14 +1,12 @@
 import matplotlib.pyplot as plt
-import numpy as np
 
 from datapreparation.file_reader import FileReader
 
 folder = "C:/Alan/Untref/ProcesamientoDeSenales/Final/Data"
 
 for index, signal in enumerate(FileReader(folder)):
-    timeline = np.linspace(0, float(len(signal.values)) / signal.fs, len(signal.values))
     fig = plt.figure(index)
     fig.canvas.set_window_title(signal.name)
-    plt.plot(timeline, signal.values)
+    plt.plot(signal.timeline, signal.values)
 
 plt.show()
--- signal/signal.py
+++ signal/signal.py
@@ -1,6 +1,16 @@
+import numpy as np
+
+
 class Signal:
 
     def __init__(self, name, values, fs):
         self.name = name
         self.values = values
         self.fs = fs
+        self._timeline = None
+
+    @property
+    def timeline(self):
+        if self._timeline is None:
+            self._timeline = np.linspace(0, float(len(self.values)) / self.fs, len(self.values))
+        return self._timeline
