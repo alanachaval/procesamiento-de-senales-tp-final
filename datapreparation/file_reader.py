bb8e40a16312519505dac6201f9992b112b26c93
Alan Achaval <achaval.alan.n@gmail.com>
2019-07-09 17:56:36 -0300

    Modificacion a FileReader para leer solo parte de la base

--- datapreparation/file_reader.py
+++ datapreparation/file_reader.py
@@ -8,11 +8,13 @@ from signal.signal import Signal
 
 class FileReader:
 
-    def __init__(self, folder, database):
+    def __init__(self, folder, database, start=''):
         self.data_folder = os.path.join(folder, database)
+        self.start = start
 
     def __iter__(self):
-        return map(self.read_file, filter(lambda x: x.endswith('.hea'), os.listdir(self.data_folder)))
+        return map(self.read_file,
+                   filter(lambda x: x.startswith(self.start) and x.endswith('.hea'), os.listdir(self.data_folder)))
 
     def read_file(self, header_name):
         path = os.path.join(self.data_folder, header_name[:-4])
--- main.py
+++ main.py
@@ -6,11 +6,11 @@ from datapreparation.file_reader import FileReader
 
 folder = sys.argv[1]
 
-databases = ['aami-ec13', 'edb', 'ltstdb', 'mitdb']
+databases = ['aami-ec13', 'edb', 'ltstdb', 'mitdb', 'nifeadb']
 
-for index, signal in enumerate(FileReader(folder, databases[0])):
+for index, signal in enumerate(FileReader(folder, databases[4], 'NR_01')):
     fig = plt.figure(index)
     fig.canvas.set_window_title(signal.record.record_name)
-    plt.plot(signal.timeline, signal.record.p_signal)
+    plt.plot(signal.timeline, signal.record.p_signal[:, 0])
 
 plt.show()
