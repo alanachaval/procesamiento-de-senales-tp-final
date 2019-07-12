ba3844bfe1cd176eda658c7008c7d563f00a6588
PaoRodrigues <paola.rodrigues@mercadolibre.com>
2019-07-11 22:18:23 -0300

    Cambio de rumbo

--- main.py
+++ main.py
@@ -1,16 +1,49 @@
 import sys
-
+import wave
 import matplotlib.pyplot as plt
+import numpy as np
+from scipy.io import wavfile
+import scipy.fftpack as sc
 
-from datapreparation.file_reader import FileReader
 
-folder = sys.argv[1]
+# señal es un array de valores
+# fs es la frecuencia de muestreo a la cual esta muestreada esa señal
+def transformadaFourier(senial, fs):
 
-databases = ['aami-ec13', 'edb', 'ltstdb', 'mitdb', 'nifeadb']
+    numero_muestras = len(senial)
 
-for index, signal in enumerate(FileReader(folder, databases[4], 'NR_01')):
-    fig = plt.figure(index)
-    fig.canvas.set_window_title(signal.record.record_name)
-    plt.plot(signal.timeline, signal.record.p_signal[:, 0])
+    # fftshift acomoda los valores de manera entendible
+    yf = sc.fft(senial)
 
-plt.show()
+    xf = sc.fftshift(np.linspace(-fs/2, fs/2, len(yf)))
+
+    return xf,yf
+
+if __name__ == "__main__":
+    
+    w = wave.open('/Users/paorodrigues/Downloads/354433__mtg__flute-b5.wav', 'r')
+
+    signal = w.readframes(-1)
+    signal = np.fromstring(signal, 'Int16')
+
+    fs = w.getframerate()
+
+    #If Stereo
+    if w.getnchannels() == 2:
+        print('Just mono files')
+        sys.exit(0)
+
+    # Time=np.linspace(0, len(signal)/fs, num=len(signal))
+    xx = np.linspace(0, len(signal)/fs, num=2000)
+
+    plt.figure(1)
+    plt.title('Signal Wave...')
+    plt.plot(xx, signal[0:2000])
+    plt.show()
+
+    xf, yf = transformadaFourier(signal[0:2000], fs)
+
+    plt.figure(2)
+    plt.title('Transformada de Fourier...')
+    plt.plot(xf, np.fft.fftshift(yf))
+    plt.show()
