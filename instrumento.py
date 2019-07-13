14b07bdf7f3bcbfe1a582b9ece626e43778db659
PaoRodrigues <paola.rodrigues@mercadolibre.com>
2019-07-13 18:55:42 -0300

    Final

--- instrumento.py
+++ instrumento.py
@@ -12,11 +12,10 @@ class Senal:
         self.tiempos = tiempos
         self.amplitudes = amplitudes
 
-    def graficar(self):
-        plt.figure(1)
-        plt.title(self.titulo)
-        plt.plot(self.tiempos, self.amplitudes)
-        plt.show()
+    def graficar(self, porcentaje=1):
+        muestras = int(porcentaje * len(self.tiempos))
+        plt.plot(self.tiempos[0:muestras], self.amplitudes[0:muestras])
+        return self
 
     def transformarFourier(self):
         numero_muestras = len(self.amplitudes)
@@ -24,19 +23,39 @@ class Senal:
         yf = sc.fftshift(sc.fft(self.amplitudes))
         xf = np.linspace(-self.fs/2, self.fs/2, len(yf))
 
-        return Senal(titulo, self.fs, xf, yf)
+        return Senal(self.titulo, self.fs, xf, np.abs(yf))
 
 
 class Instrumento:
-    def __init__(self, senales):
+    def __init__(self, senales, nombre):
         self.senales = senales
+        self.nombre = nombre
 
+    def comparar_transformada(self, fig_id):
+        plt.figure(fig_id)
+        leyenda = list()
+        for senal in self.senales:
+            leyenda.append(senal.transformarFourier().graficar().titulo)
+
+        plt.legend(leyenda, loc='upper right')
+        plt.title(self.nombre + " en Frecuencia")
+    
+    def comparar_tiempo(self, fig_id):
+        plt.figure(fig_id)
+        leyenda = list()
+        for senal in self.senales:
+            leyenda.append(senal.graficar(0.005).titulo)
+
+        plt.legend(leyenda, loc='upper right')
+        plt.title(self.nombre + " en Tiempo")
 
 class Lector:
     
     @staticmethod
     def leer_wav(archivo, titulo):
         fs, data = wavfile.read(archivo)
+        muestras = int(1*fs)
+        data = data[0:muestras]
         tiempos = np.linspace(0, len(data)/fs, num=len(data))
 
         return Senal(titulo, fs, tiempos, data)
--- main.py
+++ main.py
@@ -1,8 +1,33 @@
 import instrumento as inst
-
+import matplotlib.pyplot as plt
 
 if __name__ == "__main__":
 
+    #violin_b3 = inst.Lector.leer_wav('Notas/violin-b3.wav', "Violin B3")
+    #violin_b5 = inst.Lector.leer_wav('Notas/violin-b5.wav', "Violin B5")
     violin_c4 = inst.Lector.leer_wav('Notas/violin-c4.wav', "Violin C4")
+    violin_g3 = inst.Lector.leer_wav('Notas/violin-g3.wav', "Violin G3")
+    violin_g5 = inst.Lector.leer_wav('Notas/violin-g5.wav', "Violin G5")
+    violin = inst.Instrumento([violin_c4, violin_g3, violin_g5], "Violín")
 
-    violin_c4.graficar()
+    flauta_b5 = inst.Lector.leer_wav('Notas/flauta-b5.wav', "Flauta B5")
+    flauta_c4 = inst.Lector.leer_wav('Notas/flauta-c4.wav', "Flauta C4")
+    flauta_g5 = inst.Lector.leer_wav('Notas/flauta-g5.wav', "Flauta G5")
+    flauta = inst.Instrumento([flauta_b5, flauta_c4, flauta_g5], "Flauta")
+
+    trombon_a2 = inst.Lector.leer_wav('Notas/trombon-a2.wav', "Trombon A2")
+    trombon_c3 = inst.Lector.leer_wav('Notas/trombon-c3.wav', "Trombon C3")
+    trombon_g3 = inst.Lector.leer_wav('Notas/trombon-g3.wav', "Trombon G3")
+    trombon = inst.Instrumento([trombon_a2, trombon_c3, trombon_g3], "Trombon")
+
+    violin.comparar_transformada(1)
+    flauta.comparar_transformada(2)
+    trombon.comparar_transformada(3)
+
+    plt.show()
+
+    violin.comparar_tiempo(1)
+    flauta.comparar_tiempo(2)
+    trombon.comparar_tiempo(3)
+
+    plt.show()
