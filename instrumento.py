788c4ffce1e93f767caaec6a63c071d850be7ad8
Alan Achaval <achaval.alan.n@gmail.com>
2019-07-16 18:49:07 -0300

    Agregada guitarra, y llamadas de ploteo

--- instrumento.py
+++ instrumento.py
@@ -12,16 +12,16 @@ class Senal:
         self.tiempos = tiempos
         self.amplitudes = amplitudes
 
-    def graficar(self, porcentaje=1):
-        muestras = int(porcentaje * len(self.tiempos))
+    def graficar(self, muestras=0):
+        if muestras == 0:
+            muestras = len(self.amplitudes)
         plt.plot(self.tiempos[0:muestras], self.amplitudes[0:muestras])
         return self
 
     def transformarFourier(self):
-        numero_muestras = len(self.amplitudes)
         # fftshift acomoda los valores de manera entendible
         yf = sc.fftshift(sc.fft(self.amplitudes))
-        xf = np.linspace(-self.fs/2, self.fs/2, len(yf))
+        xf = np.linspace(-self.fs / 2, self.fs / 2, len(yf))
 
         return Senal(self.titulo, self.fs, xf, np.abs(yf))
 
@@ -39,24 +39,23 @@ class Instrumento:
 
         plt.legend(leyenda, loc='upper right')
         plt.title(self.nombre + " en Frecuencia")
-    
-    def comparar_tiempo(self, fig_id):
+
+    def comparar_tiempo(self, fig_id, muestras):
         plt.figure(fig_id)
         leyenda = list()
         for senal in self.senales:
-            leyenda.append(senal.graficar(0.005).titulo)
+            leyenda.append(senal.graficar(muestras).titulo)
 
         plt.legend(leyenda, loc='upper right')
         plt.title(self.nombre + " en Tiempo")
 
+
 class Lector:
-    
+
     @staticmethod
     def leer_wav(archivo, titulo):
         fs, data = wavfile.read(archivo)
-        muestras = int(1*fs)
+        muestras = int(1 * fs)
         data = data[0:muestras]
-        tiempos = np.linspace(0, len(data)/fs, num=len(data))
-
+        tiempos = np.linspace(0, len(data) / fs, num=len(data))
         return Senal(titulo, fs, tiempos, data)
-
--- main.py
+++ main.py
@@ -2,32 +2,75 @@ import instrumento as inst
 import matplotlib.pyplot as plt
 
 if __name__ == "__main__":
+    guitarra_b5 = inst.Lector.leer_wav('Notas/guitarra-b5.wav', "Guitarra B5")
+    guitarra_d5 = inst.Lector.leer_wav('Notas/guitarra-d5.wav', "Guitarra D5")
+    guitarra_e6 = inst.Lector.leer_wav('Notas/guitarra-e6.wav', "Guitarra E6")
+    guitarra = inst.Instrumento([guitarra_b5, guitarra_d5, guitarra_e6], "Guitarra")
 
-    #violin_b3 = inst.Lector.leer_wav('Notas/violin-b3.wav', "Violin B3")
-    #violin_b5 = inst.Lector.leer_wav('Notas/violin-b5.wav', "Violin B5")
+    # plt.figure(1)
+    # guitarra_d5.graficar(240)
+    # plt.title("Guitarra D5 en Tiempo")
+    # plt.figure(2)
+    # guitarra_d5.transformarFourier().graficar()
+    # plt.title("Guitarra D5 en Frecuencia")
+    # plt.show()
+
+    # guitarra.comparar_transformada(1)
+    # guitarra.comparar_tiempo(2, 240)
+    # plt.show()
+
     violin_c4 = inst.Lector.leer_wav('Notas/violin-c4.wav', "Violin C4")
     violin_g3 = inst.Lector.leer_wav('Notas/violin-g3.wav', "Violin G3")
     violin_g5 = inst.Lector.leer_wav('Notas/violin-g5.wav', "Violin G5")
     violin = inst.Instrumento([violin_c4, violin_g3, violin_g5], "Violín")
 
+    # plt.figure(1)
+    # violin_c4.graficar(240)
+    # plt.title("Violin C4 en Tiempo")
+    # plt.figure(2)
+    # violin_c4.transformarFourier().graficar()
+    # plt.title("Violin C4 en Frecuencia")
+    # plt.show()
+    #
+    # violin.comparar_transformada(1)
+    # violin.comparar_tiempo(2, 240)
+    # plt.show()
+
     flauta_b5 = inst.Lector.leer_wav('Notas/flauta-b5.wav', "Flauta B5")
     flauta_c4 = inst.Lector.leer_wav('Notas/flauta-c4.wav', "Flauta C4")
-    flauta_g5 = inst.Lector.leer_wav('Notas/flauta-g5.wav', "Flauta G5")
-    flauta = inst.Instrumento([flauta_b5, flauta_c4, flauta_g5], "Flauta")
+    flauta_g6 = inst.Lector.leer_wav('Notas/flauta-g6.wav', "Flauta G6")
+    flauta = inst.Instrumento([flauta_b5, flauta_c4, flauta_g6], "Flauta")
+
+    # plt.figure(1)
+    # flauta_b5.graficar(240)
+    # plt.title("Flauta B5 en Tiempo")
+    # plt.figure(2)
+    # flauta_b5.transformarFourier().graficar()
+    # plt.title("Flauta B5 en Frecuencia")
+    # plt.show()
+    #
+    # flauta.comparar_transformada(1)
+    # flauta.comparar_tiempo(2, 240)
+    # plt.show()
 
     trombon_a2 = inst.Lector.leer_wav('Notas/trombon-a2.wav', "Trombon A2")
     trombon_c3 = inst.Lector.leer_wav('Notas/trombon-c3.wav', "Trombon C3")
     trombon_g3 = inst.Lector.leer_wav('Notas/trombon-g3.wav', "Trombon G3")
     trombon = inst.Instrumento([trombon_a2, trombon_c3, trombon_g3], "Trombon")
 
-    violin.comparar_transformada(1)
-    flauta.comparar_transformada(2)
-    trombon.comparar_transformada(3)
+    # plt.figure(1)
+    # trombon_a2.graficar(240)
+    # plt.title("Trombon A2 en Tiempo")
+    # plt.figure(2)
+    # trombon_a2.transformarFourier().graficar()
+    # plt.title("Trombon A2 en Frecuencia")
+    # plt.show()
+    #
+    # trombon.comparar_transformada(1)
+    # trombon.comparar_tiempo(2, 240)
+    # plt.show()
 
-    plt.show()
-
-    violin.comparar_tiempo(1)
-    flauta.comparar_tiempo(2)
-    trombon.comparar_tiempo(3)
-
-    plt.show()
+    # comparaciones = inst.Instrumento([guitarra_d5, violin_c4, flauta_b5], "Comparaciones")
+    # comparaciones.comparar_transformada(1)
+    # comparaciones.comparar_tiempo(2, 240)
+    # plt.show()
